b436c7245108fae748d9c24f24ee46228cabc59b
Andrei Siniauski <superkenka@gmail.com>
2023-08-29 17:00:28 +0300

    01-start project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AdminDashboardComponent } from './components/admin/components/admin-dashboard/admin-dashboard.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { 
+    path: 'admin',
+    loadChildren: () => import('./components/admin/admin.module').then((m) => m.AdminModule)
+  },
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, of, throwError } from 'rxjs';
+
+interface UserInfo {
+  email: string,
+  password: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private router: Router) { }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token)
+  }
+
+  getToken() {
+    return localStorage.getItem('token')
+  }
+
+  isLoggedIn() {
+    return this.getToken() !== null
+  }
+
+  login(userInfo: UserInfo): Observable<string | boolean> {
+    if(userInfo.email === 'admin@email.com' && userInfo.password === 'admiN123') {
+      this.setToken('23u12hjdkf932kdsj39')
+      return of(true)
+    }
+    return throwError(() => new Error('Неверный логин или пороль'))
+  }
+}
